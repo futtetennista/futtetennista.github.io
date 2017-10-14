6323753fb38ee3a05cf82e4494a3bafbe7dcdda6
futtetennista <futtetennista@gmail.com>
2017-10-15 01:10:55 +0200

    A bunch of improvements towards a materialised template
    - "apt-get update" & install make in container to be able to build hlibsass

--- site.hs
+++ site.hs
@@ -1,66 +1,78 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+
 import Data.Monoid (mappend)
 import Hakyll
 import qualified Data.Set as Set
+import Hakyll.Web.Sass (sassCompiler)
 
---------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
     match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+      route   idRoute
+      compile copyFileCompiler
+
+    match "lib/**" $ do
+      route   idRoute
+      compile copyFileCompiler
+
+    match "css/*.scss" $ do
+      route $ setExtension "css"
+      let
+        compressCssItem = fmap compressCss
+      compile (compressCssItem <$> sassCompiler)
 
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+      route   idRoute
+      compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match (fromList ["about.rts", "contact.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+          >>= loadAndApplyTemplate "templates/default.html" defaultContext
+          >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompilerWith defaultHakyllReaderOptions{ readerExtensions = Set.insert (Ext_literate_haskell) (readerExtensions defaultHakyllReaderOptions) } defaultHakyllWriterOptions
+        compile $ pandocCompiler
+        -- compile $ pandocCompilerWith defaultHakyllReaderOptions{ readerExtensions = Set.insert (Ext_literate_haskell) (readerExtensions defaultHakyllReaderOptions) } defaultHakyllWriterOptions
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+    -- create ["archive.html"] $ do
+    --     route idRoute
+    --     compile $ do
+    --         posts <- recentFirst =<< loadAll "posts/*"
+    --         let archiveCtx =
+    --                 listField "posts" postCtx (return posts) `mappend`
+    --                 constField "title" "Archives"            `mappend`
+    --                 defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+    --         makeItem ""
+    --             >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+    --             >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+    --             >>= relativizeUrls
 
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+          posts <- recentFirst =<< loadAll "posts/*"
+          let
+            indexCtx =
+              listField "posts" postCtx (return posts) `mappend`
+              constField "title" "Home"                `mappend`
+              defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+          getResourceBody
+            >>= applyAsTemplate indexCtx
+            >>= loadAndApplyTemplate "templates/default.html" indexCtx
+            >>= relativizeUrls
 
-    match "templates/*" $ compile templateBodyCompiler
+    match (fromGlob "partials/*" .||. fromGlob "templates/*") $
+      compile templateBodyCompiler
 
 
---------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
