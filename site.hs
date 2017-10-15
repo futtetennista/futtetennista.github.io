66f39120ab1da32adb118cb33089b612a2c76a6f
futtetennista <futtetennista@gmail.com>
2017-10-15 22:01:01 +0200

    Formatting

--- site.hs
+++ site.hs
@@ -1,31 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
-
-import Data.Monoid ((<>))
 import Hakyll
+import Data.Monoid ((<>))
 import qualified Data.Set as Set
 import Hakyll.Web.Sass (sassCompiler)
 
 main :: IO ()
 main = hakyll $ do
-    match (fromGlob "images/**" .||. fromGlob "js/**" .||. fromGlob "lib/**") $ do
-      route   idRoute
-      compile copyFileCompiler
+  match (fromGlob "images/**" .||. fromGlob "js/**" .||. fromGlob "lib/**") $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "css/*.scss"$ do
-      route $ setExtension "css"
-      let
-        compressCssItem = fmap compressCss
-      compile (compressCssItem <$> sassCompiler)
+  match "css/*.scss"$ do
+    route $ setExtension "css"
+    let
+      compressCssItem = fmap compressCss
+    compile (compressCssItem <$> sassCompiler)
 
-    match "css/*" $ do
-      route   idRoute
-      compile compressCssCompiler
+  match "css/*" $ do
+    route   idRoute
+    compile compressCssCompiler
 
-    match (fromList ["about.rts", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-          >>= loadAndApplyTemplate "templates/default.html" defaultContext
-          >>= relativizeUrls
+  match (fromList ["about.rts", "contact.markdown"]) $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
@@ -35,41 +34,38 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    -- create ["archive.html"] $ do
-    --     route idRoute
-    --     compile $ do
-    --         posts <- recentFirst =<< loadAll "posts/*"
-    --         let archiveCtx =
-    --                 listField "posts" postCtx (return posts) `mappend`
-    --                 constField "title" "Archives"            `mappend`
-    --                 defaultContext
+  -- create ["archive.html"] $ do
+  --     route idRoute
+  --     compile $ do
+  --         posts <- recentFirst =<< loadAll "posts/*"
+  --         let archiveCtx =
+  --                 listField "posts" postCtx (return posts) `mappend`
+  --                 constField "title" "Archives"            `mappend`
+  --                 defaultContext
 
-    --         makeItem ""
-    --             >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-    --             >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-    --             >>= relativizeUrls
+  --         makeItem ""
+  --             >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+  --             >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+  --             >>= relativizeUrls
 
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-          posts <- recentFirst =<< loadAll "posts/*"
-          let
-            indexCtx =
-              listField "posts" postCtx (return posts)
-              <> field "title" (return . show . itemBody)
-              <> field "excerpt" (return . show . itemBody)
-              -- <> field "categories" (return . show . itemBody)
-              -- <> field "tags" (return . show . itemBody)
-              <> defaultContext
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let
+        indexCtx =
+          listField "posts" postCtx (return posts)
+          <> constField "title" "Home"
+          <> defaultContext
 
-          getResourceBody
-            >>= applyAsTemplate indexCtx
-            >>= loadAndApplyTemplate "templates/default.html" indexCtx
-            >>= relativizeUrls
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
 
-    match (fromGlob "partials/*" .||. fromGlob "templates/*") $
-      compile templateBodyCompiler
+  match (fromGlob "partials/*" .||. fromGlob "templates/*") $
+    compile templateBodyCompiler
 
 
 postCtx :: Context String
