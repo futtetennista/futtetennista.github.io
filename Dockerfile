099ae74514b1cbd0a816d84a1f7bc9655103e848
futtetennista <futtetennista@gmail.com>
2017-12-16 17:08:55 +0100

    Spellcheck command-line utility

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM futtetennista/hakyll:4.9.8.0-ext
 
-RUN stack upgrade && stack --resolver lts-9.14 install hlint
+RUN apt-get-install --yes hunspell
+
+RUN stack updgrade && \
+    stack --resolver lts-9.14 install hlint
 
 EXPOSE 8000
 
--- site/spellcheck.hs
+++ site/spellcheck.hs
@@ -3,54 +3,56 @@
 script
 --no-install-ghc
 --resolver lts-9.14
---package conduit-combinators,text,filepath
+--package conduit-combinators,text,filepath,mtl,turtle,system-filepath
 -}
-
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 import Conduit
 import qualified Data.Text as T
-import System.FilePath (takeExtension)
+import System.FilePath (replaceExtension)
 import System.Environment (getArgs)
+import qualified Control.Monad.State.Strict as S
+import Turtle
+import Prelude hiding (FilePath)
 
 main :: IO ()
 main = do
-  [dir] <- getArgs
-  runConduitRes $ getSpellCheckFiles dir >>= spellCheckFile >>= sinkSpellcheckFiles -- >>= runSpellchecker
+  [fp] <- getArgs
+  massageFileForSpellchecker fp
+  runSpellchecker fp
   where
-    runSpellchecker = undefined
+    mkFilePath = flip replaceExtension ".spellcheck"
 
-    sinkSpellcheckFiles Nothing = return ()
-    sinkSpellcheckFiles (Just (fp, xs)) = yield xs
-      .| mapC (T.unlines . reverse)
-      .| encodeUtf8C
-      .| sinkFile (takeWhile (/='.') fp ++ ".spellcheck")
+    massageFileForSpellchecker fp = S.evalStateT (runConduitRes $ spellCheckFile fp) False
 
-    getSpellCheckFiles dir = sourceDirectory dir
-      .| mapMC (\x -> do liftIO $ print x ; return x)
-      .| filterC (\fp -> takeExtension fp `elem` [".markdown", ".md", ".lhs"])
-      .| await
+    runSpellchecker fp = stdout (inproc "hunspell" args empty) >> sh (inproc "rm" [file] empty)
+      where
+        args = ["-d en_GB", "-w", "-p custom_dict", file]
 
-    spellCheckFile Nothing = return Nothing
-    spellCheckFile (Just fp) = sourceFile fp
+        file = T.pack $ mkFilePath fp
+
+    spellCheckFile fp = -- sourceDirectory dir
+      -- .| mapMC (\x -> liftIO $ print x >> return x)
+      -- .| filterC (\fp -> takeExtension fp `elem` [".markdown", ".md", ".lhs"])
+      -- .| awaitForever sourceFile
+      sourceFile fp
       .| decodeUtf8C
       .| mapC T.lines
-      .| foldlCE removeCodeSnippet (False, [])
-      >>= \(_, xs) -> return $ Just (fp, xs)
+      .| mapMCE removeCodeSnippet
+      .| filterCE (not . T.null)
+      .| mapC T.unlines
+      .| encodeUtf8C
+      .| sinkFile (mkFilePath fp)
 
+    removeCodeSnippet l
+      | isCodeSnippet l = S.modify' not >> return T.empty
+      | otherwise = do isCode <- S.get; return $ if isCode then T.empty else l
+            where
+              isCodeSnippet l = isCodeSnippetMd l || isCodeSnippetLhs l
 
-removeCodeSnippet :: (Bool, [T.Text]) -> T.Text -> (Bool, [T.Text])
-removeCodeSnippet (isCode, ls) l
-  | isCodeSnippet l && isCode = (False, ls)
-  | isCodeSnippet l = (True, ls)
-  | isCode = (True, ls)
-  | otherwise = (False, l:ls) -- FIX: bottle-neck
-  where
-    isCodeSnippet l = isCodeSnippetMd l || isCodeSnippetLhs l
+              isCodeSnippetMd = (=="```") . T.take 3
 
-    isCodeSnippetMd = (=="```") . T.take 3
-
-    isCodeSnippetLhs l = birdStyle l || latexStyle l
-      where
-        birdStyle = (=="> ") . T.take 2
-
-        latexStyle = (`elem`["\\begin{code}", "\\end{code}"])
+              isCodeSnippetLhs l = birdStyle l || latexStyle l
+                where
+                  birdStyle = (=="> ") . T.take 2
+                  latexStyle = (`elem`["\\begin{code}", "\\end{code}"])
